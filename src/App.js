a62e06c02afb8acc96a2be5814690bb25ca006ed
Kyle Durand <kyle.durand@shopify.com>
2017-07-04 10:04:58 -0400

    Styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,50 @@ import {TopBar} from './components';
 import './App.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentPage: 1,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(page) {
+    this.setState({currentPage: page})
+  }
+
   render() {
+    const {currentPage} = this.state;
+
     return (
       <div className="AppFrame">
-        <TopBar />
+        <TopBar
+          page={currentPage}
+          items={[
+            {
+              fill: true,
+              content: 'kyle durand',
+              handleClick: () => this.handleClick(1),
+            },
+            {
+              content: 'work',
+              handleClick: () => this.handleClick(2),
+            },
+            {
+              content: 'about',
+              handleClick: () => this.handleClick(3),
+            },
+            {
+              content: 'info',
+              handleClick: () => this.handleClick(4),
+            },
+            {
+              content: 'more',
+              handleClick: () => this.handleClick(5),
+            },
+          ]} />
       </div>
     );
   }
--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,9 +1,37 @@
-import React, {Component} from 'react';
+import React from 'react';
+import classNames from '../../utilities/class-names';
+import './TopBar.css';
 
-export default class App extends Component {
+const Page = {
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+}
+
+export default class App extends React.Component {
   render() {
+    const {items, page} = this.props;
+
     return (
-      <div>weoo</div>
+      <div className={`TopBar ${Page[page]}`}>
+        {items.map((item) => {
+          return (
+            <button
+              key={item.content}
+              className={classNames({
+                'TopBarItem': true,
+                'TopBarItem-fill': item.fill,
+              })}
+              onClick={item.handleClick}
+            >
+              {item.content}
+            </button>
+          )
+        })}
+      </div>
     );
   }
 }
+
